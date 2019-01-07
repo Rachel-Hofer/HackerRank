6af2d0ef9365dace9755e49b1545eedf12283440
Rachel Hofer <rhofer3@gmail.com>
2019-01-06 22:04:54 -0500

    done

--- easy_bonAppetit/index.js
+++ easy_bonAppetit/index.js
@@ -2,7 +2,7 @@
 
 // Anna and Brian are sharing a meal at a restuarant and they agree to split the bill equally. Brian wants to order something that Anna is allergic to though, and they agree that Anna won't pay for that item. Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
 
-// For example, assume the bill has the following prices: . Anna declines to eat item  which costs . If Brian calculates the bill correctly, Anna will pay . If he includes the cost of , he will calculate . In the second case, he should refund  to Anna.
+// For example, assume the bill has the following prices: bill = [2, 4, 6] . Anna declines to eat item  which costs k = bill[2]. If Brian calculates the bill correctly, Anna will pay (2 +4) / 2 = 3. 
 
 // Function Description
 
@@ -22,9 +22,20 @@
 
 // My Answer: (in progress)
 function bonAppetit(bill, k, b) {
-
-
-}
-
-
-// Best Practice:
+    let newBill = bill.splice(k, 1)
+    let amountCharged = bill.reduce((total, amount) => total + amount)
+    let half = amountCharged/2
+    let difference = b - half
+  
+      if(difference !== 0){
+        return difference
+      }
+      else {
+        return 'Bon Appetit'
+      }
+  }
+  
+  let bill = [3, 10, 2, 9]
+  let k = 1
+  let b = 7
+  bonAppetit(bill, k, b)
